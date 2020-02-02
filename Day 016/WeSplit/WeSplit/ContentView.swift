0a2b85b386f407a5cb1ac60bd8d9caf65bba883f
Harshal Bhavsar <hb2708@gmail.com>
2020-02-02 22:07:19 +0800

    Created new project 1 (WeSplit)

---
+++ Day 016/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Harshal Bhavsar on 02/02/2020.
+//  Copyright © 2020 Harshal Bhavsar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
