3cef5a52eca3e4fc736f6e822974bea90261d02f
Harshal Bhavsar <hb2708@gmail.com>
2020-02-02 23:21:26 +0800

    Added code to add 1. Form, 2. Sections in form 3. Navigation bar to the screen

--- Day 016/WeSplit/WeSplit/ContentView.swift
+++ Day 016/WeSplit/WeSplit/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, World!")
+                }
+                
+                Section {
+                    Text("Hello, World!")
+                }
+            }
+            .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
+        }
     }
 }
 
