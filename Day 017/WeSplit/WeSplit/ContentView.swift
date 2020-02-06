30f3c3986e428cf1b9bba97fdfe6e41535e590d4
Harshal Bhavsar <hb2708@gmail.com>
2020-02-06 22:14:24 +0800

    Finished Reading text from the user with TextField

--- Day 017/WeSplit/WeSplit/ContentView.swift
+++ Day 017/WeSplit/WeSplit/ContentView.swift
@@ -9,13 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Picker("Select your student", selection: $selectedStudent) {
-            ForEach(0..<students.count) {
-                Text(self.students[$0])
+        Form {
+            Section {
+                TextField("Amount", text:$checkAmount)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text("$\(checkAmount)")
             }
         }
     }
